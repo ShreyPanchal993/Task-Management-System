6c13f0741b1e9e46a89f79fcb6c71305c636c4c5
ShreyPanchal993 <shreyjp993@gmail.com>
2026-02-04 02:15:27 +0530

    login setup

--- Frontend/Task-Management/src/pages/Auth/Login.jsx
+++ Frontend/Task-Management/src/pages/Auth/Login.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
+import AuthLayout from "../../components/layouts/AuthLayout";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Inputs/Input";
 
 const Login = () => {
-  return(
-    <div>Login</div>
-  )
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  }
+
+  return <AuthLayout>
+    <div className="lg:w-[70%] h-3/4 md:h-full flex flex-col justify-center">
+      <h3 className="text-xl font-semibold text-black"> Welcome Back</h3>
+      <p className="text-xs text-slate-700 mt-[5px] mb-6">
+        Please enter your details to log in
+      </p>
+
+      <form onSubmit={handleLogin}>
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          label="Email"
+          type="text"
+        />
+      </form>
+
+    </div>
+  </AuthLayout>
 }
 
 export default Login
