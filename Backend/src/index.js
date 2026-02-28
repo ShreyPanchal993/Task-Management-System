6048338b090b5c9cc3ca1ed7b3f0a3a2f0a9d0fb
ShreyPanchal993 <shreyjp993@gmail.com>
2026-03-01 01:03:18 +0530

    feat: added serve upload folder for the image storing

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -2,11 +2,15 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import taskRoutes from "./routes/taskRoutes.js";
 import reportRoutes from "./routes/reportRoutes.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config({path: path.join(process.cwd(), '.env')})
 
@@ -32,6 +36,9 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/reports', reportRoutes);
+
+// Serve uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // Start Server 
 const PORT = process.env.PORT || 5000;
