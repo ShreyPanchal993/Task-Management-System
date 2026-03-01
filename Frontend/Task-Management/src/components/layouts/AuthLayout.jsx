528142bc945b9ff3289ee9368d8060fce5ab6f8e
ShreyPanchal993 <shreyjp993@gmail.com>
2026-03-01 19:23:54 +0530

    Task management system read me file

--- Frontend/Task-Management/src/components/layouts/AuthLayout.jsx
+++ Frontend/Task-Management/src/components/layouts/AuthLayout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import UI_IMG from '../../assets/images/auth-img.png';
 
 export const AuthLayout = ({ children }) => {
   return <div className="flex">
@@ -8,8 +7,7 @@ export const AuthLayout = ({ children }) => {
         {children}
       </div>
 
-      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 bg-[url('/bg-img.png')] bg-cover bg-no-repeat bg-center overflow-hidden p-8">
-        <img src={UI_IMG} className='w-64 lg:w-[90%]' />
+      <div className="hidden md:flex w-[40vw] h-screen items-center justify-center bg-blue-50 overflow-hidden p-8">
       </div>
     </div>
 }
